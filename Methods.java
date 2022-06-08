e2da375e40ad754e3effb2dde9b4f9022e8c93aa
hirokidesuga <62142935+hirokidesuga@users.noreply.github.com>
2022-06-08 22:18:09 +0900

    add method intArrayToStringArray

--- Methods.java
+++ Methods.java
@@ -1,33 +1,33 @@
 public class Methods {
     public static void main(String[] args) {
     }
-    
+
     // 最小公倍数
-    static int lcm (int a, int b) {
+    static int lcm(int a, int b) {
         if (a == 0 || b == 0) {
             System.out.println("エラー：実引数の要素に0が含まれている");
             System.exit(0);
         }
-        
+
         int temp;
         long c = a * b;
-        while((temp = a%b)!=0) {
+        while ((temp = a % b) != 0) {
             a = b;
             b = temp;
         }
 
-        return (int)(c/b);
+        return (int) (c / b);
     }
 
     // 最大公約数
-    static int gcd (int a, int b) {
+    static int gcd(int a, int b) {
         if (a == 0 || b == 0) {
             System.out.println("エラー：実引数の要素に0が含まれている");
             System.exit(0);
         }
 
         int temp;
-        while((temp = a%b)!=0) {
+        while ((temp = a % b) != 0) {
             a = b;
             b = temp;
         }
@@ -38,17 +38,34 @@ public class Methods {
     // 文字列配列を整数配列に変換
     static int[] stringArrayToIntArray(String[] stringArray) {
         int stringArrayLength = stringArray.length;
-        
+
         if (stringArrayLength <= 0) {
             System.out.println("エラー：配列の要素数が0以下");
             System.exit(0);
         }
 
         int[] intArray = new int[stringArrayLength];
-        for (int i=0; i<stringArrayLength; i++) {
+        for (int i = 0; i < stringArrayLength; i++) {
             intArray[i] = Integer.parseInt(stringArray[i]);
         }
-        
+
         return intArray;
+    }
+
+    // 整数配列を文字列配列に変換
+    static String[] intArrayToStringArray(int[] intArray) {
+        int intArrayLength = intArray.length;
+
+        if (intArrayLength <= 0) {
+            System.out.println("エラー：配列の要素数が0以下");
+            System.exit(0);
+        }
+
+        String[] strArray = new String[intArrayLength];
+        for (int i = 0; i < intArrayLength; i++) {
+            strArray[i] = String.valueOf(intArray[i]);
+        }
+
+        return strArray;
     }
 }
